c0c28c75da6ddb5f2d5c761e5eb8141cf591f31e
Christian Eder <christian.johann.eder@googlemail.com>
2021-12-06 15:09:31 +0000

    Add template

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# paws
+# PAWS - Patterns for AWS
+
+The PAWS library contains a set of reusable [level 3 constructs](https://docs.aws.amazon.com/cdk/latest/guide/constructs.html#constructs_lib) implemented in TypeScript using the AWS CDK, and published for all [JSII](https://github.com/aws/jsii)-supported ecosystems.
+
+docker run -it --rm -v C:\prj\private\paws\demo:/bla jsii/superchain bash
---
+++ packages/template/lib/index.ts
@@ -0,0 +1,16 @@
+import { Construct } from '@aws-cdk/core';
+import { Bucket, BucketEncryption, BucketProps } from '@aws-cdk/aws-s3'
+
+export class SecureBucket extends Construct {
+
+    constructor(scope: Construct, id: string, props?: BucketProps) {
+        super(scope, id);
+
+        let newProps: BucketProps = { ...props };
+        if (!props || props?.encryption === undefined || props?.encryption === BucketEncryption.UNENCRYPTED) {
+            // @ts-ignore TS2540
+            newProps.encryption = BucketEncryption.KMS_MANAGED;
+        }
+        new Bucket(this, `${id}-bucket`, newProps);
+    }
+}
