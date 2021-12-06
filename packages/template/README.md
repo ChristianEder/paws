16a0f255173420b270476760781989f36b34032e
Christian Eder <christian.johann.eder@googlemail.com>
2021-12-06 16:30:37 +0100

    Finish up template

---
+++ packages/template/README.md
@@ -0,0 +1,1 @@
+# PAWS template package
--- packages/template/lib/index.ts
+++ packages/template/lib/index.ts
@@ -1,16 +1,23 @@
-import { Construct } from '@aws-cdk/core';
+import { Construct, Tags } from '@aws-cdk/core';
 import { Bucket, BucketEncryption, BucketProps } from '@aws-cdk/aws-s3'
 
 export class SecureBucket extends Construct {
+
+    public readonly bucket: Bucket;
 
     constructor(scope: Construct, id: string, props?: BucketProps) {
         super(scope, id);
 
         let newProps: BucketProps = { ...props };
-        if (!props || props?.encryption === undefined || props?.encryption === BucketEncryption.UNENCRYPTED) {
+        const encryption = newProps?.encryption;
+
+        if (encryption === undefined || encryption === BucketEncryption.UNENCRYPTED) {
             // @ts-ignore TS2540
             newProps.encryption = BucketEncryption.KMS_MANAGED;
         }
-        new Bucket(this, `${id}-bucket`, newProps);
+
+        this.bucket = new Bucket(this, id, newProps);
+
+        Tags.of(this).add('encrypted', 'true')
     }
 }
