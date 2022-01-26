8e46a6906231aad930e2acbe8087f99d18786eb1
Christian Eder <christian.johann.eder@googlemail.com>
2022-01-26 10:03:53 +0000

    migrate to CDK v2 libs

--- packages/lambda-time-trigger/lib/index.ts
+++ packages/lambda-time-trigger/lib/index.ts
@@ -1,16 +1,11 @@
-import { Construct } from '@aws-cdk/core';
-import * as cdk from '@aws-cdk/core';
-import * as lambda from '@aws-cdk/aws-lambda';
-import * as events from '@aws-cdk/aws-events';
-import * as eventTargets from '@aws-cdk/aws-events-targets';
-import * as stepfunctions from '@aws-cdk/aws-stepfunctions';
-import * as tasks from '@aws-cdk/aws-stepfunctions-tasks';
+import { Construct } from 'constructs';
+import { Duration, aws_lambda, aws_stepfunctions, aws_stepfunctions_tasks, aws_events, aws_events_targets } from 'aws-cdk-lib';
 import * as cron from 'cron-parser';
 
 /**
  * Defines a cron expression
  */
-export interface CronOptionsWithSeconds extends events.CronOptions {
+export interface CronOptionsWithSeconds extends aws_events.CronOptions {
     /**
      * The second to run the Lambda at.
      *
@@ -25,12 +20,20 @@ export interface TimeTriggerSchedule {
     readonly cron: CronOptionsWithSeconds;
 }
 
+export interface IRulePropsTransform {
+    (ruleProps: aws_events.RuleProps): aws_events.RuleProps;
+}
+
+export interface IStateMachinePropsTransform {
+    (stateMachine: aws_stepfunctions.StateMachineProps): aws_stepfunctions.StateMachineProps;
+}
+
 /** Defines an optional set of transforms to apply to the props of resources created by the TimeTrigger */
 export interface TimeTriggerTransforms {
     /** Allows to override props for the EventBridge rule triggering the Lambda or Step Function */
-    readonly triggerRule?: (ruleProps: events.RuleProps) => events.RuleProps;
+    readonly triggerRule?: IRulePropsTransform;
     /** Allows to override props for the Step Function satte machine */
-    readonly stateMachine?: (stateMachine: stepfunctions.StateMachineProps) => stepfunctions.StateMachineProps;
+    readonly stateMachine?: IStateMachinePropsTransform;
 }
 
 export interface TimeTriggerProps {
@@ -41,12 +44,12 @@ export interface TimeTriggerProps {
     readonly transforms?: TimeTriggerTransforms;
 }
 
-export class TimeTrigger extends Construct implements lambda.IEventSource {
+export class TimeTrigger extends Construct implements aws_lambda.IEventSource {
     constructor(scope: Construct, private id: string, private props: TimeTriggerProps) {
         super(scope, id);
     }
 
-    bind(target: lambda.IFunction): void {
+    bind(target: aws_lambda.IFunction): void {
         const scopedId = this.id + '-' + target.node.id;
 
         const { second, ...restOfCronSchedule } = this.props.schedule.cron;
@@ -61,10 +64,11 @@ export class TimeTrigger extends Construct implements lambda.IEventSource {
             //   It might be better to also just ignore the value in this case, since EventBridge anyway doesn't guarantuee execution on the first second of any minute,
             //   so our queue-based solution anyway doesn't guarantuee absolute execution times, but only relative to the EventBridge execution time.
 
+            // @ts-ignore this local is unused but since this is a CDK construct, its still required since the constructor call has side effects
             const defaultScheduleRule = new events.Rule(this, scopedId + '-trigger-rule', this.transformProps({
                 enabled: true,
-                targets: [new eventTargets.LambdaFunction(target)],
-                schedule: events.Schedule.cron(restOfCronSchedule)
+                targets: [new aws_events_targets.LambdaFunction(target)],
+                schedule: aws_events.Schedule.cron(restOfCronSchedule)
             }, this.props.transforms?.triggerRule));
 
             return;
@@ -77,17 +81,17 @@ export class TimeTrigger extends Construct implements lambda.IEventSource {
         //       - Invoke without waiting for finishing
         //       - Instead of invoking directly, send message to a queue and trigger the Lambda with that
 
-        const wait = new stepfunctions.Wait(this as any, scopedId + '-wait', {
-            time: stepfunctions.WaitTime.secondsPath('$')
+        const wait = new aws_stepfunctions.Wait(this, scopedId + '-wait', {
+            time: aws_stepfunctions.WaitTime.secondsPath('$')
         });
 
-        const invoke = new tasks.LambdaInvoke(this as any, scopedId + '-invoke', {
+        const invoke = new aws_stepfunctions_tasks.LambdaInvoke(this, scopedId + '-invoke', {
             lambdaFunction: target,
-            payload: stepfunctions.TaskInput.fromObject({})
+            payload: aws_stepfunctions.TaskInput.fromObject({})
         });
 
-        const waitThenInvoke = new stepfunctions.Choice(this as any, scopedId + '-waitinvoke')
-            .when(stepfunctions.Condition.numberGreaterThan('$', 0), wait.next(invoke))
+        const waitThenInvoke = new aws_stepfunctions.Choice(this, scopedId + '-waitinvoke')
+            .when(aws_stepfunctions.Condition.numberGreaterThan('$', 0), wait.next(invoke))
             .otherwise(invoke);
 
         const seconds: number[] = [];
@@ -96,27 +100,28 @@ export class TimeTrigger extends Construct implements lambda.IEventSource {
                 seconds.push(s);
             }
         });
-        const createLoopItems = new stepfunctions.Pass(this as any, scopedId + '-loopitems', {
-            result: stepfunctions.Result.fromArray(seconds.map((s, i) => i === 0 ? s : (s - seconds[i - 1])))
+        const createLoopItems = new aws_stepfunctions.Pass(this, scopedId + '-loopitems', {
+            result: aws_stepfunctions.Result.fromArray(seconds.map((s, i) => i === 0 ? s : (s - seconds[i - 1])))
         });
 
-        const loop = new stepfunctions.Map(this as any, scopedId + '-loop', {
+        const loop = new aws_stepfunctions.Map(this, scopedId + '-loop', {
             itemsPath: '$',
             maxConcurrency: 1,
         }).iterator(waitThenInvoke);
 
         const stateMachine = createLoopItems.next(loop);
 
-        const triggerFunction = new stepfunctions.StateMachine(this as any, scopedId + '-trigger-function', this.transformProps({
-            stateMachineType: stepfunctions.StateMachineType.EXPRESS,
-            timeout: cdk.Duration.seconds(90),
+        const triggerFunction = new aws_stepfunctions.StateMachine(this, scopedId + '-trigger-function', this.transformProps({
+            stateMachineType: aws_stepfunctions.StateMachineType.EXPRESS,
+            timeout: Duration.seconds(90),
             definition: stateMachine
         }, this.props.transforms?.stateMachine));
 
+        // @ts-ignore this local is unused but since this is a CDK construct, its still required since the constructor call has side effects
         const triggerScheduleRule = new events.Rule(this, scopedId + 'trigger-rule', this.transformProps({
             enabled: true,
-            targets: [new eventTargets.SfnStateMachine(triggerFunction)],
-            schedule: events.Schedule.cron(restOfCronSchedule)
+            targets: [new aws_events_targets.SfnStateMachine(triggerFunction)],
+            schedule: aws_events.Schedule.cron(restOfCronSchedule)
         }, this.props.transforms?.triggerRule));
     }
 
--- packages/template/lib/index.ts
+++ packages/template/lib/index.ts
@@ -1,22 +1,21 @@
-import { Construct, Tags } from '@aws-cdk/core';
-import { Bucket, BucketEncryption, BucketProps } from '@aws-cdk/aws-s3'
+import { Construct, Tags, aws_s3 } from 'aws-cdk-lib';
 
 export class SecureBucket extends Construct {
 
-    public readonly bucket: Bucket;
+    public readonly bucket: aws_s3.Bucket;
 
-    constructor(scope: Construct, id: string, props?: BucketProps) {
+    constructor(scope: Construct, id: string, props?: aws_s3.BucketProps) {
         super(scope, id);
 
-        let newProps: BucketProps = { ...props };
+        let newProps: aws_s3.BucketProps = { ...props };
         const encryption = newProps?.encryption;
 
-        if (encryption === undefined || encryption === BucketEncryption.UNENCRYPTED) {
+        if (encryption === undefined || encryption === aws_s3.BucketEncryption.UNENCRYPTED) {
             // @ts-ignore TS2540
             newProps.encryption = BucketEncryption.KMS_MANAGED;
         }
 
-        this.bucket = new Bucket(this, id, newProps);
+        this.bucket = new aws_s3.Bucket(this, id, newProps);
 
         Tags.of(this).add('encrypted', 'true')
     }
