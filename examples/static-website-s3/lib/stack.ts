2e85adc61ed0e7cb1bd33acb0188dc7990bfe918
Eder <christian.eder@zuehlke.com>
2022-08-19 09:13:05 +0200

    Host static website on AWS as example

--- examples/static-website-s3/lib/stack.ts
+++ examples/static-website-s3/lib/stack.ts
@@ -1,5 +1,8 @@
 import { Stack, StackProps, Duration, aws_ecs, aws_ecs_patterns, aws_route53, aws_route53_targets, aws_certificatemanager, aws_cloudwatch, aws_cloudwatch_actions, aws_sns, aws_sns_subscriptions, RemovalPolicy } from 'aws-cdk-lib';
-import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { CachePolicy, CloudFrontWebDistribution, Distribution, OriginAccessIdentity, OriginProtocolPolicy, SecurityPolicyProtocol, SSLMethod } from 'aws-cdk-lib/aws-cloudfront';
+import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { CloudFrontTarget } from 'aws-cdk-lib/aws-route53-targets';
+import { Bucket, BucketAccessControl } from 'aws-cdk-lib/aws-s3';
 import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
 
@@ -21,22 +24,35 @@ export class StaticWebsiteS3Stack extends Stack {
       destinationBucket: bucket
     });
 
+    const originAccessIdentity = new OriginAccessIdentity(this, 'OriginAccessIdentity');
+    bucket.grantRead(originAccessIdentity);
 
-    // const hostedZone = aws_route53.HostedZone.fromLookup(this, 'some-zone', {
-    //   domainName: 'z-iot-starter-kit.click'
-    // });
+    const hostedZone = aws_route53.HostedZone.fromLookup(this, 'website-hosted-zone', {
+      domainName: 'z-iot-starter-kit.click'
+    });
+    
+    const certificate = new aws_certificatemanager.DnsValidatedCertificate(this, 'website-certificate', {
+      domainName: 'z-iot-starter-kit.click',
+      hostedZone: hostedZone,
+      region: 'us-east-1'
+    });
 
-    // const certificate = new aws_certificatemanager.Certificate(this, 'some-certificate-dev', {
-    //   domainName: 'dev.z-iot-starter-kit.click',
-    //   validation: aws_certificatemanager.CertificateValidation.fromDns(hostedZone)
-    // });
+    const distribution = new Distribution(this, 'website-distribution', {
+      defaultRootObject: 'index.html',
+      domainNames: ['z-iot-starter-kit.click'],
+      certificate: certificate,
+      defaultBehavior: {
+        cachePolicy: new CachePolicy(this, 'website-caching', {
+          defaultTtl: Duration.minutes(1)
+        }),
+        origin: new S3Origin(bucket, { originAccessIdentity }),
+      },
+    });
 
-
-    // new aws_route53.ARecord(this, 'some-a-record', {
-    //   recordName: 'dev',
-    //   zone: hostedZone,
-    //   target: aws_route53.RecordTarget.fromAlias(new aws_route53_targets.LoadBalancerTarget(service.loadBalancer)),
-    //   ttl: Duration.minutes(1)
-    // });
+    new aws_route53.ARecord(this, 'some-a-record', {
+      zone: hostedZone,
+      target: aws_route53.RecordTarget.fromAlias(new CloudFrontTarget(distribution)),
+      ttl: Duration.minutes(1),
+    });
   }
 }
